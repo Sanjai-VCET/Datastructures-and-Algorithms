bb2a70cfb130edab90b0ce98d22ffa84d7069779
Sanjai <sanjaisanjai7182@gmail.com>
2025-09-19 15:34:27 +0530

    Maximum No Of Words You Can Type

---
+++ src/String/MaximumNoOfWordsYouCanType.java
@@ -0,0 +1,11 @@
+package String;
+
+public class MaximumNoOfWordsYouCanType {
+    public static void main(String[] args) {
+
+    }
+    static int Words(String text , String brokenLetters){
+        String input1[] = text.split(" ");
+        return 5;
+    }
+}
